bd4a99c68c4078e5beff1ec798ca2bb87ad4f62f
pkachu17 <gk01095@gmail.com>
2023-12-21 21:18:48 -0800

    connect to atlas

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// require('dotenv').config({path: './env})
+import dotenv from "dotenv";
+dotenv.config({path: './env'});
+
+import connectDB from "./db/index.js";
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//***************************METHOD - 1 **************************************************/
+
+// import mongoose from 'mongoose';
+// import { DB_NAME } from "./constants";
+
+// import express from "express";
+// const app = express();
+
+// ( async () => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", () => {
+//             console.log("ERROR: ", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is running on port ${process.env.PORT}`);
+//         })
+
+//     }catch(error){
+//         console.log("ERROR: ",error);
+//         throw error
+//     }
+// })()
